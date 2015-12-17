3ece7cddad803ef97ca44c1a56ac77434699c8e8
Annabelle Ruiz <annabelle@woufbox.com>
2015-12-17 22:10:08 +0100

    integration user_pet_profile done

--- app/src/main/java/com/claire/mobileprojectspae06/UI/activities/UserPetProfile.java
+++ app/src/main/java/com/claire/mobileprojectspae06/UI/activities/UserPetProfile.java
@@ -1,20 +1,16 @@
 package com.claire.mobileprojectspae06.UI.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.claire.mobileprojectspae06.R;
 
-public class UserDogProfile extends AppCompatActivity {
+public class UserPetProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_dog_profile);
+        setContentView(R.layout.activity_user_pet_profile);
     }
 
 }
