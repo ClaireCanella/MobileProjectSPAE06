c9fdb77419eb6aed2a896666079b3eb93b6f16d6
Annabelle Ruiz <annabelle@woufbox.com>
2015-12-17 02:29:27 +0100

    create real activity for signup page

---
+++ app/src/main/java/com/claire/mobileprojectspae06/UI/activities/Signup.java
@@ -0,0 +1,21 @@
+package com.claire.mobileprojectspae06.UI.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.claire.mobileprojectspae06.R;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+
+}
